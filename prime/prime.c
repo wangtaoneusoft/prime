fe634bf7060f28b321a0f10570a2b872cbb4044b
wangtao <447087246@qq.com>
2017-07-22 00:55:46 +0800

    prime完成

--- prime/prime.c
+++ prime/prime.c
@@ -1,9 +1,51 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include<math.h>
 
 int root(int n)
 {
 	return (int)sqrt((float)n);
+}
+
+void f_Prime6(int n)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int count = 0;
+	int*p = (int*)malloc(sizeof(int)*(n + 1));
+	
+	// 初始化每一个数组元素都是1
+	for (i = 0; i < n + 1; i++)
+	{
+		p[i] = 1;
+	}
+
+	//printf("prime = 2\n");
+	count = 1;
+
+	// 最小的素数是2，所以从2开始便利，将每一个素数的倍数都置为0.则下一个不为0的数即为素数
+	for (i = 2; i <= n; i = k)
+	{
+		for (j = 2; i*j <= n; j++)
+		{
+			p[i*j] = 0;
+		}
+
+		// 查找下一个不为0的数，则该数为素数
+		for (k = i + 1; k <= n; k++)
+		{
+			if (1 == p[k])
+			{
+				//printf("prime = %d\n", k);
+				count++;
+				break;
+			}
+		}
+	}
+
+	printf("f_prime6 = %d\n",count);
 }
 
 int f_isPrime5(int n)
@@ -188,6 +230,8 @@ int main()
 	{
 		printf("f_prime%d = %d\n", j+1, count[j]);
 	}
+
+	f_Prime6(n);
 
 	return 0;
 }
